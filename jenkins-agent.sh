54721771ca434293d9ea1b4941a9cba53d941c2d
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-06-22 12:44:50 +0530

    jenkins-practice

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -1,60 +1,41 @@
 #!/bin/bash
-set -e
 
-echo "==== Resizing disk... ===="
+#resize disk from 20GB to 50GB
+growpart /dev/nvme0n1 4
 
-# Install growpart tool (for expanding partitions)
-sudo yum install -y cloud-utils-growpart
+lvextend -L +10G /dev/mapper/RootVG-homeVol
+lvextend -L +10G /dev/mapper/RootVG-varVol
+lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
 
-# Expand partition (adjust device if yours is different)
-sudo growpart /dev/nvme0n1 1
+xfs_growfs /home
+xfs_growfs /var/tmp
+xfs_growfs /var
 
-# Expand physical volume (PV)
-sudo pvresize /dev/nvme0n1p1
+yum install java-17-openjdk -y
+yum install -y yum-utils
 
-# Extend LVM logical volumes safely
-sudo lvextend -r -L +10G /dev/mapper/RootVG-homeVol || echo "homeVol not found"
-sudo lvextend -r -L +10G /dev/mapper/RootVG-varVol || echo "varVol not found"
-sudo lvextend -r -l +100%FREE /dev/mapper/RootVG-varTmpVol || echo "varTmpVol not found"
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+yum install zip -y
 
-echo "==== Installing required packages... ===="
-
-# Java
-sudo yum install -y java-17-openjdk
-
-# Terraform
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum install -y terraform
-echo 'export PATH=$PATH:/usr/bin' >> ~/.bashrc
-
-# Node.js
-sudo dnf module disable nodejs -y
-sudo dnf module enable nodejs:20 -y
-sudo dnf install -y nodejs
-
-# Zip
-sudo yum install -y zip
-
-# Docker
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
+# docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
 
 # Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod +x get_helm.sh
+chmod 700 get_helm.sh
 ./get_helm.sh
-rm -f get_helm.sh
 
-# Maven
-sudo dnf install -y maven
+# Maven for Java projects
+dnf install maven -y
 
-# Python 3.11
-sudo dnf install -y python3.11 gcc python3-devel
-
-echo "==== Disk Resize & Setup Complete ===="
-df -h
+# Python for python projects
+dnf install python3.11 gcc python3-devel -y
