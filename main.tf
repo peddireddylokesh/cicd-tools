20f38899256112c878b3b769f478b2e79f03aa5d
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-08-13 23:23:13 +0530

    “commit”

--- main.tf
+++ main.tf
@@ -1,50 +1,74 @@
+# Jenkins EC2 Instance
 module "jenkins" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "jenkins"
+  name                    = "jenkins"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = ["sg-0df48a4504a56b775"] # replace with your SG
+  subnet_id               = "subnet-0f2fcc8d17f8f9a71" # replace with your subnet
+  ami                     = data.aws_ami.ami_info.id
+  user_data               = file("jenkins.sh")
+  associate_public_ip_address = false # We will use EIP instead
 
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0df48a4504a56b775"] #replace your SG
-  subnet_id = "subnet-0f2fcc8d17f8f9a71" #replace your Subnet id used a normal one its not belongs to expense project.
-  ami = data.aws_ami.ami_info.id
-  user_data = file("jenkins.sh")
   tags = {
     Name = "jenkins"
   }
 
-  # Define the root volume size and type
   root_block_device = [
     {
-      volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+      volume_size           = 50
+      volume_type           = "gp3"
+      delete_on_termination = true
     }
   ]
 }
 
+# Elastic IP for Jenkins
+resource "aws_eip" "jenkins_eip" {
+  instance = module.jenkins.id
+  domain   = "vpc"
+
+  tags = {
+    Name = "jenkins-eip"
+  }
+}
+
+# Jenkins Agent EC2 Instance
 module "jenkins_agent" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "jenkins-agent"
+  name                    = "jenkins-agent"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = ["sg-0df48a4504a56b775"] # replace with your SG
+  subnet_id               = "subnet-0f2fcc8d17f8f9a71" # replace with your subnet
+  ami                     = data.aws_ami.ami_info.id
+  user_data               = file("jenkins-agent.sh")
+  associate_public_ip_address = false # We will use EIP instead
 
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0df48a4504a56b775"] #replace your SG
-  subnet_id = "subnet-0f2fcc8d17f8f9a71" #replace your Subnet
-  ami = data.aws_ami.ami_info.id
-  user_data = file("jenkins-agent.sh")
   tags = {
     Name = "jenkins-agent"
   }
 
   root_block_device = [
     {
-      volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+      volume_size           = 50
+      volume_type           = "gp3"
+      delete_on_termination = true
     }
   ]
 }
 
+# Elastic IP for Jenkins Agent
+resource "aws_eip" "jenkins_agent_eip" {
+  instance = module.jenkins_agent.id
+  domain   = "vpc"
+
+  tags = {
+    Name = "jenkins-agent-eip"
+  }
+}
+
+# Route53 DNS Records
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -55,21 +79,20 @@ module "records" {
     {
       name    = "jenkins"
       type    = "A"
-      ttl     = 1
+      ttl     = 60
       records = [
-        module.jenkins.public_ip
+        aws_eip.jenkins_eip.public_ip
       ]
       allow_overwrite = true
     },
     {
       name    = "jenkins-agent"
       type    = "A"
-      ttl     = 1
+      ttl     = 60
       records = [
         module.jenkins_agent.private_ip
       ]
       allow_overwrite = true
     }
   ]
-
 }
